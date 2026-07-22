33bc6198a5a02755992a0e927b5c4c18d5c60c72
chimanekaranagha-cloud <chimanekaranagha@gmail.com>
2026-07-22 21:05:03 +0530

    Add files via upload

---
+++ exp2.py
@@ -0,0 +1,41 @@
+Python 3.15.0b3 (tags/v3.15.0b3:cf16a33, Jun 23 2026, 10:03:50) [MSC v.1951 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> var=10
+>>> type(var)
+<class 'int'>
+>>> var="python"
+>>> type(var)
+<class 'str'>
+>>> var=2.5
+>>> type(var)
+<class 'float'>
+>>> var=True
+>>> type(var)
+<class 'bool'>
+>>> var1=2+3j
+>>> type(var1)
+<class 'complex'>
+>>> var=[1,2,3]
+>>> type(var)
+<class 'list'>
+>>> var=(1,2,3)
+>>> type(var)
+<class 'tuple'>
+>>> var={1,2, 3}
+>>> type(var)
+<class 'set'>
+>>> var={"name": "book"}
+>>> type(var)
+<class 'dict'>
+>>> var=range(5)
+>>> type(var)
+<class 'range'>
+>>> var=bytes([6,5,7,8])
+>>> type(var)
+<class 'bytes'>
+>>> var=bytearray([4,7,8])
+>>> type(var)
+<class 'bytearray'>
+>>> var=None
+>>> type(var)
+<class 'NoneType'>
